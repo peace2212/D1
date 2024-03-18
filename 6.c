956e5c8c2f611b0d5f9f8e5ceb0e48c633ce7dd7
Mani <159115403+peace2212@users.noreply.github.com>
2024-03-18 22:19:33 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,67 +1,90 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define max 5
-int q[max],f=-1,r=-1;
-void ins()
+#include <conio.h>
+#define SIZE 5
+int CQ[SIZE];
+int front=-1;
+int rear=-1, ch;
+int IsCQ_Full();
+int IsCQ_Empty();
+void CQ_Insert(int );
+void CQ_Delet();
+void CQ_Display();
+void main()
 {
-    if(f==(r+1)%max)
-        printf("\nQueue overflow");
-    else
-    {
-        if(f==-1)
-            f++;
-        r=(r+1)%max;
-        printf("\nEnter element to be inserted:");
-        scanf("%d",&q[r]);
-    }
+printf("1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+while(1)
+{
+int ele;
+printf("Enter your choice\n");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1: if(IsCQ_Full())
+printf("Circular Queu Overflow\n");
+else
+{
+printf("Enter the element to be inserted\n");
+scanf("%d",&ele); CQ_Insert(ele);
 }
-void del()
-{
-    if(r==-1)
-        printf("\nQueue underflow");
-    else
-    {
-        printf("\nElemnt deleted is:%d",q[f]);
-        if(f==r)
-            f=r=-1;
-        else
-            f=(f+1)%max;
-    }
+break;
+case 2: if(IsCQ_Empty())
+printf("Circular Queue Underflow\n");
+else
+CQ_Delet();
+break;
+case 3: if(IsCQ_Empty())
+printf("Circular Queue Underflow\n");
+else
+CQ_Display();
+break;
+case 4: exit(0);
 }
-void disp()
-{
-    if(f==-1)
-        printf("\nQueue empty");
-    else
-    {
-        int i;
-        printf("\nQueue elements are:\n");
-        for(i=f;i!=r;i=(i+1)%max)
-            printf("%d\t",q[i]);
-        printf("%d",q[i]);
-        printf("\nFront is at:%d\nRear is at:%d",q[f],q[r]);
-    }
 }
-int main()
+}
+void CQ_Insert(int item)
 {
-    printf("\nCircular Queue operations");
-    printf("\n1.Insert");
-    printf("\n2.Delete");
-    printf("\n3.Display");
-    printf("\n4.Exit");
-    int ch;
-    do{
-        printf("\nEnter choice:");
-        scanf("%d",&ch);
-        switch(ch)
-        {
-            case 1:ins();break;
-            case 2:del();break;
-            case 3:disp();break;
-            case 4:exit(0);
-            default:printf("\nInvalid choice...!");
-        }
-   }while(1);
-    return 0;
-
+if(front==-1)
+front++;
+rear = (rear+1)%SIZE;
+CQ[rear] =item;
+}
+void CQ_Delet()
+{
+int item; item=CQ[front];
+printf("Deleted element is: %d",item);
+front = (front+1)%SIZE;
+}
+void CQ_Display()
+{
+int i;
+if(front==-1)
+printf("Circular Queue is Empty\n");
+else
+{
+printf("Elements of the circular queue are..\n");
+for(i=front;i!=rear;
+i=(i+1)%SIZE);
+{
+printf("%d\t",CQ[i]);
+}
+printf("%d\n",CQ[i]);
+}
+}
+int IsCQ_Full()
+{
+if(front ==(rear+1)%SIZE)
+return 1;
+return 0;
+}
+int IsCQ_Empty()
+{
+if(front == -1)
+return 1;
+else if(front == rear)
+{
+printf("Deleted element is: %d",CQ[front]);
+front=-1;
+return 1;
+}
+return 0;
 }
